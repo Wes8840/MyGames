0b41f3842a624afd2a08a8b7bdd88b43433dc424
Marzanna Thief Elzanite <wesley8840@gmail.com>
2016-04-06 15:30:43 -0400

    Create app.js

---
+++ code/ajar.io/app.js
@@ -0,0 +1,65 @@
+var canvas = document.getElementById("canvas");
+var context = canvas.getContext("2d");
+var x = 250;
+var circle
+var y = 250;
+var mouseX = 250;
+var mouseY = 250;
+var velocity = 2;
+var foodPositions = [[30, 20], [400, 90], [60, 317], [300, 268]];
+
+function drawCircle(cellX, cellY, radius, color) {
+    context.beginPath();
+    context.arc(cellX, cellY, radius, 0, 2*3.14159);
+    context.fillStyle = color;
+    context.fill();
+}
+
+function clearCanvas() {
+    context.beginPath();
+    context.rect(0, 0, 500, 500);
+    context.fillStyle = "Red";
+    context.fill();
+}
+
+function calculatePosition() {
+    // For the x-axis
+    if (mouseX > x) {  
+        x = x + velocity;  // mouse is to the right of the ball
+    } else {   
+        x = x - velocity;  // mouse is to the left of the ball
+    }
+
+    // For the y-axis
+    if (mouseY > y) {
+        y = y + velocity;  // mouse is under ball
+    } else {
+        y = y - velocity;  // mouse is above ball
+    }
+
+}
+
+function drawScreen() {
+    clearCanvas();
+    calculatePosition();
+    drawCircle(foodPositions[0][0], foodPositions[0][1], 10, "Green");
+    drawCircle(foodPositions[1][0], foodPositions[1][1], 10, "Grey");
+    drawCircle(foodPositions[2][0], foodPositions[2][1], 10, "Cyan");
+    drawCircle(foodPositions[3][0], foodPositions[3][1], 10, "Yellow");
+    drawCircle(x, y, 40, "Blue");
+    setTimeout(drawScreen, 1000/60);
+    
+    if (circle.overlap(foodPositions)) {
+    foodPositions.remove
+
+}
+
+}
+
+function mouseMoved(e) {
+    mouseX = e.clientX;
+    mouseY = e.clientY;
+}
+
+canvas.addEventListener("mousemove", mouseMoved);
+drawScreen();
